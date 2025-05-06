57679cf5077780dd7fc77a3f8fd98fc60022cbe2
Manish Kumar Dohare <167315854+manishdohare1250@users.noreply.github.com>
2025-05-06 17:10:05 +0530

    Update and rename _95_fibonacci_series_upto_certain_number.java to _095_fibonacci_series_upto_certain_number.java

--- _095_fibonacci_series_upto_certain_number.java
+++ _095_fibonacci_series_upto_certain_number.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class _95_fibonacci_series_upto_certain_number {
+public class _095_fibonacci_series_upto_certain_number {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter  number: ");
