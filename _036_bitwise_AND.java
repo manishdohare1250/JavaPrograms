37279ecf6d89030bf4be87c765341c3ad8bf5461
Manish Kumar Dohare <167315854+manishdohare1250@users.noreply.github.com>
2025-05-07 00:26:12 +0530

    Update and rename _36_bitwise_AND.java to _036_bitwise_AND.java

--- _036_bitwise_AND.java
+++ _036_bitwise_AND.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class _36_bitwise_AND {
+public class _036_bitwise_AND {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter first number: ");
