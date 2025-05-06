9bfce391c3814b9dd8c80a650a7cd50595591dfa
Manish Kumar Dohare <167315854+manishdohare1250@users.noreply.github.com>
2025-05-06 16:42:12 +0530

    Update and rename _13_Addiition_of_2_numbers_using_user_input.java to _013_Addiition_of_2_numbers_using_user_input.java

--- _013_Addiition_of_2_numbers_using_user_input.java
+++ _013_Addiition_of_2_numbers_using_user_input.java
@@ -1,6 +1,6 @@
 // addition of two numbers with user input
 import java.util.*;
-public class _13_Addiition_of_2_numbers_using_user_input {
+public class _013_Addiition_of_2_numbers_using_user_input {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
