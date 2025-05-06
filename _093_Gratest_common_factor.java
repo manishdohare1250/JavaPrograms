84f94436418bf6d5a8447a1da997e66190ece0dd
Manish Kumar Dohare <167315854+manishdohare1250@users.noreply.github.com>
2025-05-06 17:11:03 +0530

    Update and rename _93_Gratest_common_factor.java to _093_Gratest_common_factor.java

--- _093_Gratest_common_factor.java
+++ _093_Gratest_common_factor.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class _93_Gratest_common_factor {
+public class _093_Gratest_common_factor {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter first number: ");
