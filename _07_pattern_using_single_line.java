1aa5870976bf516cf51edf9168ca68043d2d583c
Manish Kumar Dohare <167315854+manishdohare1250@users.noreply.github.com>
2025-05-06 16:19:59 +0530

    Update and rename _7_pattern_using_single_line.java to _07_pattern_using_single_line.java

--- _07_pattern_using_single_line.java
+++ _07_pattern_using_single_line.java
@@ -1,5 +1,5 @@
 // print pattern usxing \n
-public class _7_pattern_using_single_line {
+public class _07_pattern_using_single_line {
     public static void main(String[] args) {
         System.out.println("*\n**\n***\n****\n*****");
        System.out.println("next");
