ff8a2265f69393e38e351c369f3ed91254ce2842
Manish Kumar Dohare <167315854+manishdohare1250@users.noreply.github.com>
2025-05-06 16:24:54 +0530

    Update and rename _12_input.java to _012_input.java

--- _012_input.java
+++ _012_input.java
@@ -1,5 +1,5 @@
 //create a program to input name of the person and repond with "Welcome NAME to Kg"
-import java.util.*;public class _12_input {
+import java.util.*;public class _012_input {
     public static void main(String[]args){
 Scanner sc=new Scanner (System.in);
 System.out.print("Enter your name:");
