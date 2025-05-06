c81723d4506883080624dbe4092ef2f948937199
Manish Kumar Dohare <167315854+manishdohare1250@users.noreply.github.com>
2025-05-07 00:28:27 +0530

    Update and rename _40__left_shift.java to _040__left_shift.java

--- _040__left_shift.java
+++ _040__left_shift.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class _40__left_shift {
+public class _040__left_shift {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter first number: ");
