f6d1408574809a7f57f67e3e39b6fba74461a3c4
Manish Kumar Dohare <167315854+manishdohare1250@users.noreply.github.com>
2025-05-06 16:51:11 +0530

    Update and rename _16_Area_of_a_circle.java to _016_Area_of_a_circle.java

--- _016_Area_of_a_circle.java
+++ _016_Area_of_a_circle.java
@@ -1,6 +1,6 @@
 // area of a circle by taking radius as input
 import java.util.*;
-public class _16_Area_of_a_circle {
+public class _016_Area_of_a_circle {
     public static void main(String[] args) {
         Scanner k= new Scanner (System.in);
         System.out.println("enter radius of a circle:");
