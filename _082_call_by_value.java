55fa5f3742572c69b03cc343e8129b85cb088fb6
Manish Kumar Dohare <167315854+manishdohare1250@users.noreply.github.com>
2025-05-07 00:22:57 +0530

    Update and rename _82_call_by_value.java to _082_call_by_value.java

--- _082_call_by_value.java
+++ _082_call_by_value.java
@@ -2,7 +2,7 @@
 // in call by value  --"the value of variables in main function remains same always,.
 //                       the value of variable  is only updated for the function which we are called.
 
-public class _82_call_by_value {
+public class _082_call_by_value {
     public static void main(String[] args) {
         int a=10;
         System.out.println("Before calling <changeA> function value of <a>(in <<main function>>) is:"+a);
