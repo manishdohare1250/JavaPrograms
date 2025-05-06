9939de84bf441a88a2c9424367057c03c204c51f
Manish Kumar Dohare <167315854+manishdohare1250@users.noreply.github.com>
2025-05-06 16:54:58 +0530

    Update and rename _97_palindrome_or_not.java to _097_palindrome_or_not.java

--- _097_palindrome_or_not.java
+++ _097_palindrome_or_not.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class _97_palindrome_or_not {
+public class _097_palindrome_or_not {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter  number: ");
