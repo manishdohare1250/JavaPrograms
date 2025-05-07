8965955c36065cc0714ce4ed4e7b7e77d7c40eb4
Manish Kumar Dohare <167315854+manishdohare1250@users.noreply.github.com>
2025-05-07 11:18:22 +0530

    Update and rename _76_print_half_pyramid_usin_aplhabet_characters.java to _076_print_half_pyramid_usin_aplhabet_characters.java

--- _076_print_half_pyramid_usin_aplhabet_characters.java
+++ _076_print_half_pyramid_usin_aplhabet_characters.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class _76_print_half_pyramid_usin_aplhabet_characters {
+public class _076_print_half_pyramid_usin_aplhabet_characters {
     public static void main(String[] args) {
         Scanner k=new Scanner(System.in);
         System.out.println("Enter number:");
