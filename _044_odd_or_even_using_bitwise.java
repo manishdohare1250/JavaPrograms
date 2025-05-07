bd9fe04e554d0247378ace9d9f29e526a9e8eb70
Manish Kumar Dohare <167315854+manishdohare1250@users.noreply.github.com>
2025-05-07 11:07:13 +0530

    Update and rename _44_odd_or_even_using_bitwise.java to _044_odd_or_even_using_bitwise.java

--- _044_odd_or_even_using_bitwise.java
+++ _044_odd_or_even_using_bitwise.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class _44_odd_or_even_using_bitwise {
+public class _044_odd_or_even_using_bitwise {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter Your number: ");
