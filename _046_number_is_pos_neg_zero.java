c91fedebd009dd9383c8cf44b11071a6cb4520f8
Manish Kumar Dohare <167315854+manishdohare1250@users.noreply.github.com>
2025-05-07 11:08:11 +0530

    Update and rename _46_number_is_pos_neg_zero.java to _046_number_is_pos_neg_zero.java

--- _046_number_is_pos_neg_zero.java
+++ _046_number_is_pos_neg_zero.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class _46_number_is_pos_neg_zero {
+public class _046_number_is_pos_neg_zero {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter  number: ");
