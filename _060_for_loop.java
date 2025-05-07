29e03764f72115a6283a003d960365c2a68d8663
Manish Kumar Dohare <167315854+manishdohare1250@users.noreply.github.com>
2025-05-07 11:16:12 +0530

    Update and rename _60_for_loop.java to _060_for_loop.java

--- _060_for_loop.java
+++ _060_for_loop.java
@@ -1,6 +1,6 @@
 // wap to print hello world 10 times using for loop
 
-public class _60_for_loop {
+public class _060_for_loop {
     public static void main(String[] args) {
        for(int count=1;count<=10;count++){
         System.out.println("hello world");
