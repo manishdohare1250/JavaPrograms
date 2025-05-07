f44c19fec9e346b190af208e944456243b1c4ef6
Manish Kumar Dohare <167315854+manishdohare1250@users.noreply.github.com>
2025-05-07 11:11:51 +0530

    Update and rename _52_Switchcase_vowel_consonant.java to _052_Switchcase_vowel_consonant.java

--- _052_Switchcase_vowel_consonant.java
+++ _052_Switchcase_vowel_consonant.java
@@ -1,6 +1,6 @@
 // SWITCH CASE--check entered  alphabet character is vowel or consonant
 import java.util.*;
-public class _52_Switchcase_vowel_consonant {//GOOD PRACTICE OF WRITING CLASS NAME --class name should  start with capital letter,,, ex- Switchcase
+public class _052_Switchcase_vowel_consonant {//GOOD PRACTICE OF WRITING CLASS NAME --class name should  start with capital letter,,, ex- Switchcase
 
     public static void main(String[] args) {
         Scanner  k = new Scanner(System.in);
