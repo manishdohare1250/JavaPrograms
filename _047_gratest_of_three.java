14dd51382ca5fbf98887972a2b4d378f9c69b7a3
Manish Kumar Dohare <167315854+manishdohare1250@users.noreply.github.com>
2025-05-07 11:08:50 +0530

    Update and rename _47_gratest_of_three.java to _047_gratest_of_three.java

--- _047_gratest_of_three.java
+++ _047_gratest_of_three.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class _47_gratest_of_three {
+public class _047_gratest_of_three {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter first number: ");
