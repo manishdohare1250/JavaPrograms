c5376de62821fddb47e0338d2e63c7b728736489
Manish Kumar Dohare <167315854+manishdohare1250@users.noreply.github.com>
2025-05-07 11:26:14 +0530

    Update and rename _63_for_loop_alphabet.java to _063_for_loop_alphabet.java

--- _063_for_loop_alphabet.java
+++ _063_for_loop_alphabet.java
@@ -1,5 +1,5 @@
 // wap to print alphabet
-public class _63_for_loop_alphabet {
+public class _063_for_loop_alphabet {
     public static void main(String[] args) {
         for(char ch='a';ch<='z';ch++){
             System.out.print(ch+" ");
