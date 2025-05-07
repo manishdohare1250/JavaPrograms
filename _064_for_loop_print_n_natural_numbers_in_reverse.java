34950b743d774bb020cb4d9573d4bd1b0e1c74af
Manish Kumar Dohare <167315854+manishdohare1250@users.noreply.github.com>
2025-05-07 11:25:47 +0530

    Update and rename _64_for_loop_print_n_natural_numbers_in_reverse.java to _064_for_loop_print_n_natural_numbers_in_reverse.java

--- _064_for_loop_print_n_natural_numbers_in_reverse.java
+++ _064_for_loop_print_n_natural_numbers_in_reverse.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class _64_for_loop_print_n_natural_numbers_in_reverse {
+public class _064_for_loop_print_n_natural_numbers_in_reverse {
     public static void main(String[] args) {
         Scanner k= new Scanner(System.in);
         System.out.println("enter value of n:");
