08d4beb732230ea3b86b166f7005f58235713478
Manish Kumar Dohare <167315854+manishdohare1250@users.noreply.github.com>
2025-05-07 11:26:47 +0530

    Update and rename _62_for_loop_sum.java to _062_for_loop_sum.java

--- _062_for_loop_sum.java
+++ _062_for_loop_sum.java
@@ -1,6 +1,6 @@
 // wap to print sum of first 10 natural numbers
 import java.util.*;
-public class _62_for_loop_sum {
+public class _062_for_loop_sum {
     public static void main(String[] args) {
         Scanner m =new Scanner(System.in);
         System.out.println("enter value of n:");
