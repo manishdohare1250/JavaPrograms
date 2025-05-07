56d1f5525f8123a6b4c6167f5901cb8c23202841
Manish Kumar Dohare <167315854+manishdohare1250@users.noreply.github.com>
2025-05-07 11:09:45 +0530

    Update and rename _49_calculate_grades.java to _049_calculate_grades.java

--- _049_calculate_grades.java
+++ _049_calculate_grades.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class _49_calculate_grades {
+public class _049_calculate_grades {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter marks: ");
